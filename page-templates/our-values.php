c4f172a912830b39c95ac2144b2faa2b2a61c430
Joy Ling <joy@joys-mbp.home>
2020-03-30 16:03:56 +0100

    Added charity block to Our Values page

--- page-templates/our-values.php
+++ page-templates/our-values.php
@@ -15,6 +15,28 @@ get_header(); ?>
 
 <?php include(get_template_directory() . '/global-templates/template-parts/hero-banner.tpl'); ?>
 
+<section class="generic bg-white">
+	<div class="charity-wrapper">
+		<h1><?php the_field('charity_headline'); ?></h1>
+		<p><?php the_field('charity_intro'); ?></p>
+		<div class="container">
+			<div class="row">
+				<?php if( have_rows('charity_logos') ):
+						
+					while ( have_rows('charity_logos') ) : the_row();
+						$charity_logo_images = get_sub_field('charity_logo_images');
+						$charity_links = get_sub_field('charity_links');
+					?>					
+						<div class="col-6 col-md-3">
+							<a href="<?php echo $charity_links['url']?>" target="<?php echo $charity_links['target']?>"><img src="<?php echo $charity_logo_images['url']; ?>" class="charity-logo"></a>
+						</div>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include(get_template_directory() . '/global-templates/template-parts/case-studies.tpl'); ?>
 
 <?php include(get_template_directory() . '/global-templates/template-parts/banner-cta.tpl'); ?>
