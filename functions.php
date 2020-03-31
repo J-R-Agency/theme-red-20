3f89a1539c84800cb9079187467cebf0a9edcb2b
Joy Ling <joy@joys-mbp.home>
2020-03-31 15:37:41 +0100

    Add excerpt to pages

--- functions.php
+++ functions.php
@@ -136,3 +136,6 @@ function add_taxonomies_to_pages() {
  register_taxonomy_for_object_type( 'category', 'page' );
  }
 add_action( 'init', 'add_taxonomies_to_pages' );
+
+// Add excerpts to pages
+add_post_type_support( 'page', 'excerpt' );
