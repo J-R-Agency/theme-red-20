e0c9178b4efcfc31114769b75a1b873777fe6aca
Joy Ling <joy@joys-mbp.home>
2020-04-01 13:22:33 +0100

    Worked on blog post

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -14,25 +14,41 @@ defined( 'ABSPATH' ) || exit;
 	<header class="entry-header">
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<div class="post-excerpt">
+			<?php the_excerpt(); ?>
+		</div>
+		<div class="social-share">
+			social sharing buttons here
+		</div>
 
 	</header><!-- .entry-header -->
 
-	
-
-	<div class="entry-content">
-		TEST
+	<div class="post-content">
 		<?php the_content(); ?>
 
-		<?php
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-
 	</div><!-- .entry-content -->
-
+	
+	<div class="post-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-3">
+			    <?php
+			    		$categories = get_the_category();
+		 
+						if ( ! empty( $categories )) {
+						    $san_cat = sanitize_title( $categories[0]->name );  
+						}
+			    ?>	
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/logos/<?php echo $san_cat; ?>-logo-color.png">
+				</div>
+				<div class="col-9" style="display:flex;">
+					<div class="category-description">
+						<strong><?php echo esc_html( $categories[0]->name ); ?></strong>
+						<?php $catID = get_the_category(); echo category_description( $catID [0] ); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </article><!-- #post-## -->
--- single.php
+++ single.php
@@ -28,8 +28,42 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'loop-templates/content', 'single' ); ?>
-
-					<?php understrap_post_nav(); ?>
+							
+							<div class="post-navigation">
+								<?php
+									$post_id = $post->ID; // current post ID
+									$caseStudy = get_category_by_slug('case-study');
+												
+									$args = array( 
+									    'category__not_in' => $caseStudy,
+									    'orderby'  => 'post_date',
+									    'order'    => 'DESC'
+									);
+									$posts = get_posts( $args );
+									// get IDs of posts retrieved from get_posts
+									$ids = array();
+									foreach ( $posts as $thepost ) {
+									    $ids[] = $thepost->ID;
+									}
+									// get and echo previous and next post in the same category
+									$thisindex = array_search( $post_id, $ids );
+									$previd    = isset( $ids[ $thisindex - 1 ] ) ? $ids[ $thisindex - 1 ] : 0;
+									$allid     = isset( $ids[ $thisindex] ) ? $ids[ $thisindex] : 0;
+									$nextid    = isset( $ids[ $thisindex + 1 ] ) ? $ids[ $thisindex + 1 ] : 0;
+									
+									if ( $previd ) {
+									    ?><a id="prev" rel="prev" href="<?php echo get_permalink($previd) ?>">Previous news article</a>
+									    <?php
+									}
+									if ( $allid ) {
+										?><a id="all" href="<?php echo site_url();?>/news" class="link"><div class="btn_red-border">View All News</div></a><?php
+									}
+									
+									if ( $nextid ) {
+									    ?><a id="next" rel="next" href="<?php echo get_permalink($nextid) ?>">Next news article</a><?php
+									}
+								?>
+							</div>
 
 				<?php endwhile; // end of the loop. ?>
 
