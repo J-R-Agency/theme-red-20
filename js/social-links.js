3052b70566fda71df6eec092f628d1e8b607888c
Joy Ling <joy@joys-mbp.home>
2020-04-06 15:03:05 +0100

    Jetpack

--- js/social-links.js
+++ js/social-links.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
 	
-	$("a.share-facebook").append("<img src='/assets/images/facebook-red.png'>");
-	$("a.share-twitter").append("<img src='/assets/images/twitter-red.png'>");
-	$("a.share-linkedin").append("<img src='/assets/images/linkedin-red.png'>");
+	$("a.share-facebook").append("<img src='<?php echo site_url();?>/assets/images/facebook-red.png'>");
+	$("a.share-twitter").append("<img src='<?php echo site_url();?>/assets/images/twitter-red.png'>");
+	$("a.share-linkedin").append("<img src='<?php echo site_url();?>/assets/images/linkedin-red.png'>");
 });
