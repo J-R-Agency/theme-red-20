cc2ff654d7991bef42b1066b3d95906bc6ea2e25
Joy Ling <joy@joys-mbp.home>
2020-04-06 15:23:52 +0100

    Jetpack

--- js/social-links.js
+++ js/social-links.js
@@ -1,12 +1,10 @@
 jQuery(document).ready(function($) {
 	var root = document.location.hostname;
 	
-	$("a.share-facebook").append("<img id='facebook-icon-red'>");
-	$('#facebook-icon-red').attr("src","<?php echo site_url(); ?>/assets/images/facebook-red.png");
+	$("a.share-facebook").append("<div id='facebook-icon-red'>");
+	//$('#facebook-icon-red').attr("src","<?php echo site_url(); ?>/assets/images/facebook-red.png");
 	
-	$("a.share-twitter").append("<img id='twitter-icon-red'>");
-	$('#twitter-icon-red').attr("src","<?php echo site_url(); ?>/assets/images/twitter-red.png");
+	$("a.share-twitter").append("<div id='twitter-icon-red'>");
 	
-	$("a.share-linkedin").append("<img id='linkedin-icon-red'>");
-	$('#linkedin-icon-red').attr("src","<?php echo site_url(); ?>/assets/images/linkedin-red.png");
+	$("a.share-linkedin").append("<div id='linkedin-icon-red'>");
 });
