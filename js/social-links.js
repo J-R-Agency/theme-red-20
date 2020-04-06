c469881a20f1585eed73488295ce3319a43ee19d
Joy Ling <joy@joys-mbp.home>
2020-04-06 14:58:02 +0100

    Jetpack icons

--- js/social-links.js
+++ js/social-links.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) {
-	$("a.share-facebook").append("<p>Facebook</p>");
-	$("a.share-twitter").append("<p>Twitter</p>");
-	$("a.share-linkedin").append("<p>Linkedin</p>");
+	$("a.share-facebook").append("<img src='../assets/images/facebook-red.png'>");
+	$("a.share-twitter").append("<img src='../assets/images/twitter-red.png'>");
+	$("a.share-linkedin").append("<img src='../assets/images/linkedin-red.png'>");
 });
