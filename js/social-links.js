db20a2f90cc028e067a8c0e1eb7f5942eac5cf75
Joy Ling <joy@joys-mbp.home>
2020-04-06 15:02:00 +0100

    Jetpack fix

--- js/social-links.js
+++ js/social-links.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
-	$("a.share-facebook").append("<img src='../assets/images/facebook-red.png'>");
-	$("a.share-twitter").append("<img src='../assets/images/twitter-red.png'>");
-	$("a.share-linkedin").append("<img src='../assets/images/linkedin-red.png'>");
+	
+	$("a.share-facebook").append("<img src='/assets/images/facebook-red.png'>");
+	$("a.share-twitter").append("<img src='/assets/images/twitter-red.png'>");
+	$("a.share-linkedin").append("<img src='/assets/images/linkedin-red.png'>");
 });
