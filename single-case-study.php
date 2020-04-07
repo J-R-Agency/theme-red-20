2de18469e42a0efd1854fd02769ece4d4cfdf260
Joy Ling <joy@joys-mbp.home>
2020-04-07 16:22:15 +0100

    Added content for case study post

--- single-case-study.php
+++ single-case-study.php
@@ -13,6 +13,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <?php include(get_template_directory() . '/global-templates/template-parts/hero-banner-post.tpl'); ?>
+
+<section class="generic bg-white">
+	<div class="three-column">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>	
+				<?php the_content(); ?>
+			<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</section>
+
 <?php include(get_template_directory() . '/global-templates/template-parts/our-involvement.tpl'); ?>
 <?php include(get_template_directory() . '/global-templates/template-parts/photo-gallery.tpl'); ?>
 <?php include(get_template_directory() . '/global-templates/template-parts/banner-cta.tpl'); ?>
