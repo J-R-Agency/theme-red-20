f93d2d65ab55861af9dac1bf4633b493fd90cec2
Joy Ling <joy@joys-mbp.home>
2020-04-08 14:44:21 +0100

    Social link fix

--- js/social-links.js
+++ js/social-links.js
@@ -12,6 +12,6 @@ jQuery(document).ready(function($) {
 	$(".sharedaddy").eq(1).remove();
 		
 	$(".share-icon").click(function() {
-		window.open(this.href);
+		window.open(this.href, 'width=300,height=250');
 	});
 });
