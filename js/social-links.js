749d947fd588bf6d05f3462052d6834bba2baf87
Joy Ling <joy@joys-mbp.home>
2020-04-08 14:35:29 +0100

    Social link fix

--- js/social-links.js
+++ js/social-links.js
@@ -4,11 +4,12 @@ jQuery(document).ready(function($) {
 	$(".sd-title").append("<div id='share-icon'>");
 	
 	$("a.share-facebook").append("<div id='facebook-icon-red'>");
-	//$('#facebook-icon-red').attr("src","<?php echo site_url(); ?>/assets/images/facebook-red.png");
 	
 	$("a.share-twitter").append("<div id='twitter-icon-red'>");
 	
 	$("a.share-linkedin").append("<div id='linkedin-icon-red'>");
 	
 	$(".sharedaddy").eq(1).remove();
+	
+	$(".share-icon").delegate("a", "onclick", "window.open(this.href); return false;");
 });
